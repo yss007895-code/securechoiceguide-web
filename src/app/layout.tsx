71aef80b13cb83543979ceb346eb2134113ed55b
bjunseog60-boop <yss007895@gmail.com>
2026-02-22 03:49:16 +0000

    Add GA4 tracking to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-KSLL4L883T" strategy="afterInteractive" />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-KSLL4L883T');`}
+        </Script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link rel="preconnect" href="https://www.googletagmanager.com" />
